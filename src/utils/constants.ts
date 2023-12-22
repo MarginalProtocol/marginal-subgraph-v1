c1724d8899fc995d694f83130ef12d3e79cfea73
999yugen <151792546+999yugen@users.noreply.github.com>
2023-12-22 08:31:15 -0800

    Construct util constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
 import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
-import 
+import { MarginalV1Factory } from '../../generated/MarginalV1Factory/MarginalV1Factory'
+import { MarginalV1NonfungiblePositionManager } from './../../generated/MarginalV1NonfungiblePositionManager/MarginalV1NonfungiblePositionManager';
+
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
 export let ZERO_BD = BigDecimal.fromString('0')
@@ -7,8 +9,15 @@ export let ONE_BD = BigDecimal.fromString('1')
 export let BI_18 = BigInt.fromI32(18)
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = ''
-export const QUOTER_ADDRESS = ''
-export const NFT_POSITION_MANAGER_ADDRESS = ''
 
-export let factoryContract = 
+// Core Contract
+export const FACTORY_ADDRESS = '0xb92eb4ffC52a0EC383efa6577b2c3c89B7b980f5'
+
+// Periphery Write Functions
+export const NFT_POSITION_MANAGER_ADDRESS = '0x95D95C41436C15b50217Bf1C0f810536AD181C13'
+
+// Periphery View Functions
+export const QUOTER_ADDRESS = '0x7385197B160CA2ec114960BCb32bc5D7CfA2720c'
+
+export let factoryContract = MarginalV1Factory.bind(Address.fromString(FACTORY_ADDRESS))
+export let nftPositionManagerContract = MarginalV1NonfungiblePositionManager.bind(Address.fromString(NFT_POSITION_MANAGER_ADDRESS))
