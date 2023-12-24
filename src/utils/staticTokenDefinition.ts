f566672691dc9c266e74561bc14c7a184e621604
999yugen <151792546+999yugen@users.noreply.github.com>
2023-12-24 07:50:34 -0800

    Import token/helpers util functions

---
+++ src/utils/staticTokenDefinition.ts
@@ -0,0 +1,22 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+
+// Initialize a Token Definition with the attributes
+export class StaticTokenDefinition {
+  address: Address;
+  symbol: string;
+  name: string;
+  decimals: BigInt;
+
+  // Initialize a Token Definition with its attributes
+  constructor(
+    address: Address,
+    symbol: string,
+    name: string,
+    decimals: BigInt
+  ) {
+    this.address = address;
+    this.symbol = symbol;
+    this.name = name;
+    this.decimals = decimals;
+  }
+}
