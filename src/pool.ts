a5d721194484434edf8fa65881d90be47a7ba02b
999yugen <151792546+999yugen@users.noreply.github.com>
2023-12-26 08:51:48 -0800

    handleOpen function to index

--- src/pool.ts
+++ src/pool.ts
@@ -1,14 +1,29 @@
+import { Position } from "../generated/schema"
 import {
   Adjust,
   Liquidate,
   Open,
   Settle,
 } from "../generated/templates/MarginalV1Pool/MarginalV1Pool"
-import { loadPool } from "./utils/loaders"
+import { loadPool, loadTransaction } from "./utils/loaders"
 
 export function handleOpen(event: Open): void {
   let pool = loadPool(event, event.address)
-  let sender = event.params.sender
+  let transaction = loadTransaction(event)
 
-  
+  let sender = event.params.sender.toHexString()
+  let positionId = event.params.id.toHexString()
+
+  let position = new Position(positionId) as Position
+
+  position.owner = sender
+  position.pool = pool.id
+  position.margin = event.params.margin
+  position.blockNumber = event.block.number
+  position.timestamp = event.block.timestamp
+  position.transaction = transaction.id
+
+  pool.save()
+  transaction.save()
+  position.save()
 }
