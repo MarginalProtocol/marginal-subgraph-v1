5e84ac664670f9a7a6f3d07da3b9980112868b36
999yugen <151792546+999yugen@users.noreply.github.com>
2024-01-02 18:53:50 -0800

    Save Open entity in `handleOpen`

--- src/pool.ts
+++ src/pool.ts
@@ -1,14 +1,14 @@
-import { Position } from "../generated/schema"
+import { Position, Open } from "../generated/schema"
 import {
-  Adjust,
-  Liquidate,
-  Open,
-  Settle,
+  Adjust as AdjustEvent,
+  Liquidate as LiquidateEvent,
+  Open as OpenEvent,
+  Settle as SettleEvent,
 } from "../generated/templates/MarginalV1Pool/MarginalV1Pool"
 import { loadPool, loadTransaction } from "./utils/loaders"
 import { ONE_BI } from "./utils/constants"
 
-export function handleOpen(event: Open): void {
+export function handleOpen(event: OpenEvent): void {
   let pool = loadPool(event, event.address)
   let transaction = loadTransaction(event)
 
@@ -24,9 +24,20 @@ export function handleOpen(event: Open): void {
   position.timestamp = event.block.timestamp
   position.transaction = transaction.id
 
+  let open = new Open(positionId) as Open
+  open.transaction = transaction.id
+  open.timestamp = transaction.timestamp
+  open.pool = pool.id
+  open.token0 = pool.token0
+  open.token1 = pool.token1
+  open.owner = event.params.owner
+  open.sender = event.params.sender
+  open.margin = event.params.margin
+
   pool.txCount = pool.txCount.plus(ONE_BI)
-  
+
   pool.save()
   transaction.save()
   position.save()
+  open.save()
 }
