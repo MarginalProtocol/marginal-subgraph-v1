fd3e869d5c4c4a4bf07830b51d413363074be62f
999yugen <151792546+999yugen@users.noreply.github.com>
2024-01-26 16:52:10 -0800

    Index Mint event from position manager

--- src/pool.ts
+++ src/pool.ts
@@ -8,45 +8,45 @@ import {
 import { loadPool, loadTransaction, loadPosition } from "./utils/loaders"
 import { ONE_BI } from "./utils/constants"
 
-export function handleOpen(event: OpenEvent): void {
-  let pool = loadPool(event, event.address)
-  let transaction = loadTransaction(event)
+// export function handleOpen(event: OpenEvent): void {
+//   let pool = loadPool(event, event.address)
+//   let transaction = loadTransaction(event)
 
-  let sender = event.params.sender.toHexString()
-  let positionId = event.params.id.toHexString()
-  let owner = event.params.owner.toHexString()
+//   let sender = event.params.sender.toHexString()
+//   let positionId = event.params.id.toHexString()
+//   let owner = event.params.owner.toHexString()
   
-  // Create unique id using pool and positionId
-  let _positionId = pool.id.concat('-').concat(positionId)
+//   // Create unique id using pool and positionId
+//   let _positionId = pool.id.concat('-').concat(positionId)
   
-  let position = new Position(_positionId) as Position
-  position.owner = owner
-  position.pool = pool.id
-  position.margin = event.params.margin
-  position.blockNumber = event.block.number
-  position.timestamp = event.block.timestamp
-  position.transaction = transaction.id
-  position.isLiquidated = false
-  position.isSettled = false
-  position.isClosed = false
+//   let position = new Position(_positionId) as Position
+//   position.owner = owner
+//   position.pool = pool.id
+//   position.margin = event.params.margin
+//   position.blockNumber = event.block.number
+//   position.timestamp = event.block.timestamp
+//   position.transaction = transaction.id
+//   position.isLiquidated = false
+//   position.isSettled = false
+//   position.isClosed = false
 
-  let open = new Open(positionId) as Open
-  open.transaction = transaction.id
-  open.timestamp = transaction.timestamp
-  open.pool = pool.id
-  open.token0 = pool.token0
-  open.token1 = pool.token1
-  open.owner = event.params.owner
-  open.sender = event.params.sender
-  open.margin = event.params.margin
+//   let open = new Open(positionId) as Open
+//   open.transaction = transaction.id
+//   open.timestamp = transaction.timestamp
+//   open.pool = pool.id
+//   open.token0 = pool.token0
+//   open.token1 = pool.token1
+//   open.owner = event.params.owner
+//   open.sender = event.params.sender
+//   open.margin = event.params.margin
 
-  pool.txCount = pool.txCount.plus(ONE_BI)
+//   pool.txCount = pool.txCount.plus(ONE_BI)
 
-  pool.save()
-  transaction.save()
-  position.save()
-  open.save()
-}
+//   pool.save()
+//   transaction.save()
+//   position.save()
+//   open.save()
+// }
 
 // TODO: Need to rethink through how to index multiple Adjust events to a Position
 // export function handleAdjust(event: AdjustEvent): void {
@@ -73,30 +73,30 @@ export function handleOpen(event: OpenEvent): void {
 //   adjust.save()
 // } 
 
-export function handleSettle(event: SettleEvent): void {
-  let transaction = loadTransaction(event)
-  let pool = loadPool(event, event.address)
+// export function handleSettle(event: SettleEvent): void {
+//   let transaction = loadTransaction(event)
+//   let pool = loadPool(event, event.address)
 
-  let positionId = event.params.id.toHexString()
-  let _positionId = pool.id.concat('-').concat(positionId)
+//   let positionId = event.params.id.toHexString()
+//   let _positionId = pool.id.concat('-').concat(positionId)
 
-  let position = loadPosition(event, event.params.owner, pool, _positionId)
-  position.isSettled = true
-  position.isClosed = true
+//   let position = loadPosition(event, event.params.owner, pool, _positionId)
+//   position.isSettled = true
+//   position.isClosed = true
 
-  let settle = new Settle(positionId) as Settle
-  settle.transaction = transaction.id
-  settle.timestamp = transaction.timestamp
-  settle.pool = pool.id
-  settle.token0 = pool.token0
-  settle.token1 = pool.token1
-  settle.owner = event.params.owner
-  settle.recipient = event.params.recipient
-  settle.rewards = event.params.rewards
+//   let settle = new Settle(positionId) as Settle
+//   settle.transaction = transaction.id
+//   settle.timestamp = transaction.timestamp
+//   settle.pool = pool.id
+//   settle.token0 = pool.token0
+//   settle.token1 = pool.token1
+//   settle.owner = event.params.owner
+//   settle.recipient = event.params.recipient
+//   settle.rewards = event.params.rewards
   
-  position.save()
-  settle.save()
-}
+//   position.save()
+//   settle.save()
+// }
 
 // export function handleLiquidate(event: LiquidateEvent): void {
 //   let transaction = loadTransaction(event)
--- src/positionManager.ts
+++ src/positionManager.ts
@@ -1,7 +1,34 @@
 import {
   Mint as MintEvent,
 } from "../generated/MarginalV1NonfungiblePositionManager/MarginalV1NonfungiblePositionManager"
+import { loadPool, loadTransaction, loadPosition } from "./utils/loaders"
+import { MarginalV1NonfungiblePositionManager } from "../generated/MarginalV1NonfungiblePositionManager/MarginalV1NonfungiblePositionManager"
+import { NFT_POSITION_MANAGER_ADDRESS } from "./utils/constants"
+import { Position } from "../generated/schema"
+import { Address } from "@graphprotocol/graph-ts"
 
 export function handleMint(event: MintEvent): void {
-  
+  let transaction = loadTransaction(event)
+
+  let positionManagerContract = MarginalV1NonfungiblePositionManager.bind(event.address)
+
+  let tokenId = event.params.tokenId
+  let position = new Position(tokenId.toHexString())
+
+  const positionInfo = positionManagerContract.positions(tokenId)
+  let poolAddress: Address = positionInfo[0]
+
+  position.pool = positionInfo[0]
+  position.owner = event.params.recipient.toHexString()
+  position.margin = event.params.margin
+  position.blockNumber = event.block.number
+  position.timestamp = event.block.timestamp
+  position.transaction = transaction.id
+  position.isLiquidated = false
+  position.isSettled = false
+  position.isClosed = false
+
+
+  position.save()
+  transaction.save()
 }
