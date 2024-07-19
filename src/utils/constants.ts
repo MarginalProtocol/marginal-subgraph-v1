a9d004f1c279fed2a682abd833b9f013c54d8541
999yugen <151792546+999yugen@users.noreply.github.com>
2024-07-19 09:06:24 -0700

    mainnet addresses

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,13 +11,13 @@ export let BI_18 = BigInt.fromI32(18)
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 // Uniswap V3 Factory Contract
-export const UNI_V3_FACTORY_ADDRESS = '0x33128a8fC17869897dcE68Ed026d694621f6FDfD'
+export const UNI_V3_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 
 // Core Contract
-export const FACTORY_ADDRESS = '0x407fA8029852A8386a907287018CEAFd7242C621'
+export const FACTORY_ADDRESS = '0x95D95C41436C15b50217Bf1C0f810536AD181C13'
 
 // Periphery Write Functions
-export const NFT_POSITION_MANAGER_ADDRESS = '0x12B4F1984Ef15f93b5636E3332eF4fF727EAa79d'
+export const NFT_POSITION_MANAGER_ADDRESS = '0x273Fb953a410126436F252a86a98c9eff4d2C780'
 
 export let factoryContract = MarginalV1Factory.bind(Address.fromString(FACTORY_ADDRESS))
 export let nftPositionManagerContract = MarginalV1NonfungiblePositionManager.bind(Address.fromString(NFT_POSITION_MANAGER_ADDRESS))
