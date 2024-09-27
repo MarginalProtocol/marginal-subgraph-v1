8bfed6b3b08d1d14d978acf95d12b4a38d9d6b42
Cryptara <admin@cryptara.io>
2024-09-27 14:22:04 +0200

    Other factory handlers

--- src/marginal-v-1-lb-factory.ts
+++ src/marginal-v-1-lb-factory.ts
@@ -21,33 +21,28 @@ import { loadLBFactory } from "./utils/loaders"
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/token"
 
 export function handleCollectProtocol(event: CollectProtocolEvent): void {
-  let entity = new MarginalV1LBFactoryCollectProtocol(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.sender = event.params.sender
-  entity.token = event.params.token
-  entity.recipient = event.params.recipient
-  entity.amount = event.params.amount
+  // let entity = new MarginalV1LBFactoryCollectProtocol(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32()),
+  // )
+  // entity.sender = event.params.sender
+  // entity.token = event.params.token
+  // entity.recipient = event.params.recipient
+  // entity.amount = event.params.amount
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
 
-  entity.save()
+  // entity.save()
 }
 
 export function handleOwnerChanged(event: OwnerChangedEvent): void {
-  let entity = new MarginalV1LBFactoryOwnerChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.oldOwner = event.params.oldOwner
-  entity.newOwner = event.params.newOwner
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let factory = loadLBFactory(event.address.toHexString())
 
-  entity.save()
+  factory.owner = event.params.newOwner.toHexString()
+
+  factory.save()
 }
 
 export function handlePoolCreated(event: PoolCreatedEvent): void {
@@ -114,15 +109,11 @@ export function handlePoolCreated(event: PoolCreatedEvent): void {
 }
 
 export function handleSetFeeProtocol(event: SetFeeProtocolEvent): void {
-  let entity = new MarginalV1LBFactorySetFeeProtocol(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.oldFeeProtocol = event.params.oldFeeProtocol
-  entity.newFeeProtocol = event.params.newFeeProtocol
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let factory = loadLBFactory(event.address.toHexString())
 
-  entity.save()
+  factory.feeProtocol = event.params.newFeeProtocol
+
+  factory.save()
+
 }
